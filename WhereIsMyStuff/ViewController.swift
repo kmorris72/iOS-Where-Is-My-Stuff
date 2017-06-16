c668e1904bb073bafc87ca92b268b47b6f412fbe
Kipp Morris <kipp.morris@gmail.com>
2017-06-16 08:53:09 -0400

    Added basic UI elements to welcome screen (no functionality)

--- WhereIsMyStuff/ViewController.swift
+++ WhereIsMyStuff/ViewController.swift
@@ -20,14 +20,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBOutlet weak var sampleButton: UIButton!
-
-    @IBAction func clicked(_ sender: Any) {
-        if let title = sampleButton.currentTitle {
-            if title == "Sample" {
-                sampleButton.setTitle("Clicked!", for: UIControlState.normal)
-            }
-        }
-    }
 }
-
