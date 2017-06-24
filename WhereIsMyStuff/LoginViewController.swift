99864ea869c3283db778329628cade239894f12b
Kipp Morris <kipp.morris@gmail.com>
2017-06-24 12:39:52 -0400

    Added LoginViewController and created outlets for text fields and an action for the login button

---
+++ WhereIsMyStuff/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  WhereIsMyStuff
+//
+//  Created by Kipp Morris on 6/24/17.
+//  Copyright © 2017 Fiveloop. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onLoginButtonClick(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: "Hello", preferredStyle: UIAlertControllerStyle.alert)
+        present(alert, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
