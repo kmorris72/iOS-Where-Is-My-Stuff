e965963e94fcf3fca8abd10707e91870e44cc510
Kipp Morris <kipp.morris@gmail.com>
2017-06-27 18:49:06 -0400

    Completed RegisterViewController

--- WhereIsMyStuff/RegisterViewController.swift
+++ WhereIsMyStuff/RegisterViewController.swift
@@ -35,7 +35,65 @@ class RegisterViewController: UIViewController {
     }
 
     @IBAction func onRegisterButtonClick(_ sender: Any) {
-        let thing = RegisterViewController.model.addUser(firstName: firstName.text, lastName: lastName.text, email: email.text, username: username.text, password1: password1.text, password2: password2.text, isAdmin: _adminSwitch.isOn)
+        let code = RegisterViewController.model.addUser(firstName: firstName.text, lastName: lastName.text, email: email.text, username: username.text, password1: password1.text, password2: password2.text, isAdmin: _adminSwitch.isOn)
+        if (code == 0) {
+            let alert = UIAlertController(title: nil, message: "User Added!", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: {(alert: UIAlertAction!) in self.performSegue(withIdentifier: "Register -> Welcome", sender: self)})
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+            // intent code remember
+        } else if (code == 1) {
+            let alert = UIAlertController(title: nil, message: "Name Invalid", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else if (code == 2) {
+            let alert = UIAlertController(title: nil, message: "Email Invalid", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else if (code == 3) {
+            let alert = UIAlertController(title: nil, message: "Username Invalid", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else if (code == 4) {
+            let alert = UIAlertController(title: nil, message: "Username Cannot Contain Whitespace Characters", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else if (code == 5) {
+            let alert = UIAlertController(title: nil, message: "Username Cannot Contain '@' Character", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        }  else if (code == 6) {
+            let alert = UIAlertController(title: nil, message: "Password Invalid", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else if (code == 7) {
+            let alert = UIAlertController(title: nil, message: "Passwords Do Not Match", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else if (code == 8) {
+            let alert = UIAlertController(title: nil, message: "Email Already In Database", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else if (code == 9) {
+            let alert = UIAlertController(title: nil, message: "Username Alerady In Database", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: nil, message: "User NOT Added", preferredStyle: UIAlertControllerStyle.alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        }
+
     }
     
 }
