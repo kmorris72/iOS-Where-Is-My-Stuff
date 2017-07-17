534ceda9aa11a228cdedac833214b240a233d007
Kipp Morris <kipp.morris@gmail.com>
2017-07-17 17:49:50 -0400

    Restructured model so that UserManager and ItemManager are separate classes for OO design purposes

---
+++ WhereIsMyStuff/ItemManager.swift
@@ -0,0 +1,98 @@
+//
+//  ItemManager.swift
+//  WhereIsMyStuff
+//
+//  Created by Kipp Morris on 7/17/17.
+//  Copyright © 2017 Fiveloop. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ItemManager {
+    private var _lostItems: Dictionary<String, Model.Item>
+    private var _foundItems: Dictionary<String, Model.Item>
+    private static let instance = ItemManager()
+    
+    private let databaseRef = Database.database().reference()
+    
+    private let _lostItemsDatabase: DatabaseReference
+    private let _foundItemsDatabase: DatabaseReference
+    
+    private init() {
+        _lostItems = Dictionary<String, Model.Item>()
+        _foundItems = Dictionary<String, Model.Item>()
+        _lostItemsDatabase = databaseRef.child("lost items")
+        _foundItemsDatabase = databaseRef.child("found items")
+    }
+    
+    static func getInstance() -> ItemManager {
+        return instance
+    }
+    
+    func addLostItem(name: String, typePosition: Int, description: String, user: Model.User) {
+        let type = Model.ItemType.values[typePosition]
+        let item = Model.Item(name: name, type: type, description: description, user: user)
+        _lostItems.updateValue(item, forKey: name)
+        let itemMirror = Mirror(reflecting: item)
+        let itemRef = _lostItemsDatabase.child(name)
+        for (label, value) in itemMirror.children {
+            switch value {
+            case is Model.ItemType:
+                itemRef.child(label!.replacingOccurrences(of: "_", with: "")).setValue((value as! Model.ItemType).rawValue)
+            case is Model.User:
+                let userMirror = Mirror(reflecting: value as! Model.User)
+                for (label, value) in userMirror.children {
+                    itemRef.child("user").child(label!.replacingOccurrences(of: "_", with: "")).setValue(value)
+                }
+            default:
+                itemRef.child(label!.replacingOccurrences(of: "_", with: "")).setValue(value)
+            }
+        }
+    }
+    
+    func addFoundItem(name: String, typePosition: Int, description: String, user: Model.User) {
+        let type = Model.ItemType.values[typePosition]
+        let item = Model.Item(name: name, type: type, description: description, user: user)
+        _foundItems.updateValue(item, forKey: name)
+        let itemMirror = Mirror(reflecting: item)
+        let itemRef = _foundItemsDatabase.child(name)
+        for (label, value) in itemMirror.children {
+            switch value {
+            case is Model.ItemType:
+                itemRef.child(label!.replacingOccurrences(of: "_", with: "")).setValue((value as! Model.ItemType).rawValue)
+            case is Model.User:
+                let userMirror = Mirror(reflecting: value as! Model.User)
+                for (label, value) in userMirror.children {
+                    itemRef.child("user").child(label!.replacingOccurrences(of: "_", with: "")).setValue(value)
+                }
+            default:
+                itemRef.child(label!.replacingOccurrences(of: "_", with: "")).setValue(value)
+            }
+        }
+    }
+    
+    func getLostItems() -> Array<Model.Item> {
+        return Array(_lostItems.values)
+    }
+    
+    func getFoundItems() -> Array<Model.Item> {
+        return Array(_foundItems.values)
+    }
+    
+    func searchFound(foundItem: Bool, name: String) -> Bool {
+        if (foundItem) {
+            return _foundItems[name] != nil
+        } else {
+            return _lostItems[name] != nil
+        }
+    }
+    
+    func searchResult(foundItem: Bool, name: String) -> String {
+        if (foundItem) {
+            return _foundItems[name]!.getDescription()
+        } else {
+            return _lostItems[name]!.getDescription()
+        }
+    }
+}
---
+++ WhereIsMyStuff/UserManager.swift
@@ -0,0 +1,176 @@
+//
+//  UserManager.swift
+//  WhereIsMyStuff
+//
+//  Created by Kipp Morris on 7/17/17.
+//  Copyright © 2017 Fiveloop. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class UserManager {
+    private var _currentUser: Model.User!
+    private var _users: Dictionary<String, Model.User>
+    private var _emailUser: Dictionary<String, String>
+    private static let instance = UserManager()
+
+    private let databaseRef = Database.database().reference()
+    
+    private let _usersDatabase: DatabaseReference
+    
+    private init() {
+        _users = Dictionary<String, Model.User>()
+        _emailUser = Dictionary<String, String>()
+        _usersDatabase = databaseRef.child("users")
+        setUp()
+    }
+    
+    static func getInstance() -> UserManager {
+        return instance
+    }
+    
+    private func setUpAddUser(user: Model.User) {
+        _users[user.getUsername()] = user
+    }
+    
+    private func setUp() {
+        addUser(firstName: "admin", lastName: "one", email: "admin@gatech.edu", username: "user", password1: "pass", password2: "pass", isAdmin: true)
+        _usersDatabase.observe(DataEventType.value, with: { (snapshot) in
+            print(self._users)
+        })
+    }
+    
+    private func validateInput(firstName: String?, lastName: String?, email: String?, username: String?, password1: String?, password2: String?) -> Int {
+        if let firstName = firstName, let lastName = lastName {
+            if (firstName.characters.count == 0 || lastName.characters.count == 0) {
+                return 1
+            }
+            if let email = email {
+                if (email.characters.count == 0 || email.range(of: "@") == nil) {
+                    return 2
+                } else if let emailUser = _emailUser[email] {
+                    return 8
+                }
+                if let username = username {
+                    if (username.characters.count == 0) {
+                        return 3
+                    } else if (username.range(of: " ") != nil) {
+                        return 4
+                    } else if (username.range(of: "@") != nil) {
+                        return 5
+                    } else if let user = _users[username] {
+                        return 9
+                    }
+                } else {
+                    return 3
+                }
+                if let password1 = password1, let password2 = password2 {
+                    if (password1.characters.count == 0 || password2.characters.count == 0) {
+                        return 6
+                    } else if (!(password1 == password2)) {
+                        return 7
+                    }
+                } else {
+                    return 6
+                }
+            } else {
+                return 2
+            }
+        } else {
+            return 1
+        }
+        return 0
+    }
+    
+    func addUser(firstName: String?, lastName: String?, email: String?, username: String?, password1: String?, password2: String?, isAdmin: Bool) -> Int {
+        let code = validateInput(firstName: firstName, lastName: lastName, email: email, username: username, password1: password1, password2: password2)
+        if (code == 0) {
+            let firstName = firstName!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastName!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = email!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let username = username!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let newUser = Model.User(firstName: firstName, lastName: lastName, email: email, username: username, password: password1!, isAdmin: isAdmin)
+            _users.updateValue(newUser, forKey: username)
+            _emailUser.updateValue(username, forKey: email)
+            _currentUser = newUser
+            
+            // may still need to add name field to database (firstname and lastName separated by a space) bc they have it in theirs
+            let userRef = self._usersDatabase.child(username)
+            let userMirror = Mirror(reflecting: _currentUser as Model.User)
+            for (label, value) in userMirror.children {
+                userRef.child(label!.replacingOccurrences(of: "_", with: "")).setValue(value)
+            }
+        }
+        return code
+    }
+    
+    func loginUser(usernameEmail: String?, password: String?) -> Int {
+        if let _usernameEmail = usernameEmail, let _password = password {
+            if (_usernameEmail.characters.count == 0 || _password.characters.count == 0) {
+                return 1
+            }
+            let user: Model.User?
+            let username: Bool
+            if (_usernameEmail.range(of: "@") == nil) {
+                user = _users[_usernameEmail]
+                username = true
+            } else {
+                user = _users[_emailUser[_usernameEmail]!]
+                username = false
+            }
+            if (_usernameEmail.characters.count == 0 || password?.characters.count == 0) {
+                return 1
+            }
+            else if let user = user {
+                if (!user.checkPassword(password: _password)) {
+                    return 4
+                } else {
+                    _currentUser = user
+                    return 0
+                }
+            } else if username {
+                return 2
+            } else {
+                return 3
+            }
+        } else {
+            return 1
+        }
+    }
+    
+    func getName() -> String {
+        if let _currentUser = _currentUser {
+            return _currentUser.getName()
+        } else {
+            return "Anonymous"
+        }
+    }
+    
+    func getCurrentUser() -> Model.User {
+        return _currentUser
+    }
+    
+    //        mutating func addLostItem(name: String?, typePosition: String?, description: String?) -> Int {
+    //            //Can the Spinner be a string?
+    //            let _name: String?
+    //            let _typePosition: String?
+    //            let _description: String?
+    //
+    //            if (name == nil || typePosition == nil || description == nil) {
+    //
+    //                return 1
+    //            } else {
+    //                _name = name
+    //                _typePosition = typePosition
+    //                _description = description
+    //            }
+    //
+    //            if (_name!.characters.count == 0 ||  _typePosition!.characters.count == 0 || _description!.characters.count == 0) {
+    //
+    //                return 1
+    //            }
+    //
+    //            return 0;
+    //        }
+}
