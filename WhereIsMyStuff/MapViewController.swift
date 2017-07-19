d49d4fc2652602076dd752812d069178301b874f
Rahul <rahul.brahmal@me.com>
2017-07-19 12:23:52 -0400

    Added the Map View Controller and MapViewController.swift class. Basic functionality added for both.

---
+++ WhereIsMyStuff/MapViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MapViewController.swift
+//  WhereIsMyStuff
+//
+//  Created by Rahul Brahmal on 7/19/17.
+//  Copyright © 2017 Fiveloop. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
